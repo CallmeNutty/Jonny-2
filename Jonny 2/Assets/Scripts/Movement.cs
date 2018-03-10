b59cf66551ac44a49016bda990f97fde53b239ba
TheNuttyGamer <33847045+TheNuttyGamer@users.noreply.github.com>
2018-03-10 16:56:31 +0000

    Began PC

---
+++ Jonny 2/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.velocity = new Vector2(1 * movementSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.velocity = new Vector2(-1 * movementSpeed, 0);
+        }
+    }
+}
---
+++ Jonny 2/Assets/Scripts/PC.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC : MonoBehaviour
+{
+    private bool inZone;
+
+    void OnTriggerEnter2D(Collider2D coll) {inZone = true;}
+
+    void OnTriggerExit2D(Collider2D coll) {inZone = false;}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inZone == true)
+        {
+            print("Wetfarts");
+        }
+    }
+}
